97365195d16a1c8890b6da336eeb98a9c250a9c0
Gyrkora <giselle.gallardo0210@gmail.com>
2023-03-28 19:23:10 -0300

    adding styled-components, formik and swiper

--- frontend/frontend-hells-comedy/src/App.js
+++ frontend/frontend-hells-comedy/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.scss';
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+			<swiper-container>
+				<swiper-slide>Slide 1</swiper-slide>
+				<swiper-slide>Slide 2</swiper-slide>
+				<swiper-slide>Slide 3</swiper-slide>
+				<swiper-slide>Slide ...</swiper-slide>
+			</swiper-container>
 		</div>
 	);
 }
