981da7150ebe12490037332c48d3e67ea1a46cb2
hadaroadams <hadaroadams1234@gmail.com>
2023-04-01 20:57:45 +0000

    header Phase 1

---
+++ frontend/frontend-hells-comedy/src/styles/Header.style.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+const Head = styled.header`
+    display: flex;
+    background: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeSXOkxolChIYrCRO7dcZrhwGc99iSIHV_F9hTY_8KQTKMT0Ra-oOzDgtsp-ITyuNsBvM&usqp=CAU') no-repeat ;
+    background-size: 100%;
+    width: 100%;
+    height: 100px; 
+
+`
+
+
+
+
+
+
+
+export {Head}
